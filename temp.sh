b3e1173dd171636a8b77ebfec3d385000569d37f
be-great <begreat5333@gmail.com>
2025-01-11 18:56:08 +0300

    update file

--- temp.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "# MedifyCare_new" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:EsraMohammednour/MedifyCare_new.git
-git push -u origin main
